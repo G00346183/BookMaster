554a158d7a04edb65e86b95a8e09fa214e6cf9bf
unknown <G00346183@gmit.ie>
2019-11-29 09:15:41 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatInputModule, MatButtonModule, MatCardModule, MatToolbarModule} from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateComponent } from './create/create.component';
+import { DeleteComponent } from './delete/delete.component';
+import { EditComponent } from './edit/edit.component';
+import { ReadComponent } from './read/read.component';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent,
+    EditComponent,
+    ReadComponent,
+    DeleteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    HttpClientModule,
+    MatListModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/book.model.ts
@@ -0,0 +1,7 @@
+export class Book {
+    title: string;
+    year: string;
+    poster: string;
+    isbn : string;
+}
+
